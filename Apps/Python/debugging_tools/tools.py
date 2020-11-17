4353388c5bdcff5efac7280bb5fa376bd7a47e4d
Gregory Kramida <algomorph@gmail.com>
2020-11-17 13:00:30 -0500

    Add python tools to check segment-box intersections (for debugging)

--- Apps/Python/debugging_tools/tools.py
+++ Apps/Python/debugging_tools/tools.py
@@ -3,41 +3,70 @@ from typing import Mapping, Tuple
 
 
 class Segment:
+    def __init__(self, start: np.ndarray, end: np.ndarray):
+        self.start = np.array(start)
+        self.end = np.array(end)
+        self.direction = self.end - self.start
+        self.inverse_direction = 1 / self.direction
+        self.sign = self.inverse_direction < 0.0
+
+    def __str__(self):
+        return "start: " + str(self.start) + ", end: " + str(self.end)
+
+    def __repr__(self):
+        return "start: " + repr(self.start) + ", end: " + repr(self.end)
 
 
-def segment_sign(segment: Tuple[np.ndarray, np.ndarray]):
-    direction = segment[1] - segment[0]
-    return (1.0 / direction) < 0.0
+class GridAlignedBox:
+    def __init__(self, min_point: np.ndarray, max_point: np.ndarray):
+        self.min_point = min_point
+        self.max_point = max_point
+        self.bounds = (min_point, max_point)
+
+    def __str__(self):
+        return "min_point: " + str(self.min_point) + ", max_point: " + str(self.max_point)
+
+    def __repr__(self):
+        return "min_point: " + repr(self.min_point) + ", max_point: " + repr(self.max_point)
 
 
-def SegmentIntersectsGridAlignedBox3D(segment: Tuple[np.ndarray, np.ndarray],
-                                      boxMin: Mapping[int, float], boxMax: Mapping[int, float]):
-    tmin = tmax = tymin = tymax = tzmin = tzmax = 0.
-    bounds = (boxMin, boxMax)
-    sign = segment_sign(segment)
+def get_voxel_block_extent_metric(block_coordinates: np.ndarray, voxel_size=0.004, voxel_block_size=8):
+    return GridAlignedBox(np.array(block_coordinates, dtype=np.float) * voxel_block_size * voxel_size - voxel_size / 2,
+                          (np.array(block_coordinates, dtype=np.float) + np.array([1, 1, 1]) * voxel_block_size - voxel_size / 2))
 
-    tmin = (bounds[sign[0]].x - segment.origin.x) * segment.inverseDirection.x;
-    tmax = (bounds[1 - sign[0]].x - segment.origin.x) * segment.inverseDirection.x;
-    tymin = (bounds[sign[1]].y - segment.origin.y) * segment.inverseDirection.y;
-    tymax = (bounds[1 - sign[1]].y - segment.origin.y) * segment.inverseDirection.y;
 
-    if ((tmin > tymax) or (tymin > tmax)):
+def get_voxel_block_extent_blocks(block_coordinates: np.ndarray):
+    return GridAlignedBox(np.array(block_coordinates, dtype=np.float), (np.array(block_coordinates, dtype=np.float) + np.array([1, 1, 1])))
+
+
+def segment_intersects_grid_aligned_box(segment: Segment, box: GridAlignedBox):
+    bounds = box.bounds
+    sign = segment.sign
+    start = segment.start
+    inverse_direction = segment.inverse_direction
+
+    t_min = (bounds[sign[0]][0] - start[0]) * inverse_direction[0]
+    t_max = (bounds[1 - sign[0]][0] - start[0]) * inverse_direction[0]
+    t_y_min = (bounds[sign[1]][1] - start[1]) * inverse_direction[1]
+    t_y_max = (bounds[1 - sign[1]][1] - start[1]) * inverse_direction[1]
+
+    if (t_min > t_y_max) or (t_y_min > t_max):
         return False
 
-    if (tymin > tmin):
-        tmin = tymin
-    if (tymax < tmax):
-        tmax = tymax
+    if t_y_min > t_min:
+        t_min = t_y_min
+    if t_y_max < t_max:
+        t_max = t_y_max
 
-    tzmin = (bounds[segment.sign[2]].z - segment.origin.z) * segment.inverseDirection.z;
-    tzmax = (bounds[1 - segment.sign[2]].z - segment.origin.z) * segment.inverseDirection.z;
+    t_z_min = (bounds[segment.sign[2]][2] - start[2]) * segment.inverse_direction[2]
+    t_z_max = (bounds[1 - segment.sign[2]][2] - start[2]) * segment.inverse_direction[2]
 
-    if ((tmin > tzmax) | | (tzmin > tmax)):
+    if (t_min > t_z_max) or (t_z_min > t_max):
         return False
 
-    if (tzmin > tmin):
-        tmin = tzmin
+    if t_z_min > t_min:
+        t_min = t_z_min
 
-    if (tzmax < tmax):
-        tmax = tzmax
-    return not (tmax < 0.0 or tmin > 1.0)
+    if t_z_max < t_max:
+        t_max = t_z_max
+    return not (t_max < 0.0 or t_min > 1.0)
